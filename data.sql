dcbf3d2d75f3dc58fede9460ccaf97d48e918785
meghnakr <54601079+meghnakr@users.noreply.github.com>
2022-10-30 13:28:20 -0400

    Completed schema. Started data.sql file.

---
+++ data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Media VALUES (1, "Star Wars: Episode IV - A New Hope", '2022-11-01', NULL, 'PG', 'English',
+    'Science Fiction', 105, 1, "Movie", 1977);
+
+INSERT INTO Company VALUES(1, 'Disney', "USA");
